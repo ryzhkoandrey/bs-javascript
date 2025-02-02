fa6a43f75bcdd040a8876e787920c687e1d916eb
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-02 22:10:32 +0200

    8

--- tasks/08-function-expression.js
+++ tasks/08-function-expression.js
@@ -10,3 +10,11 @@
  *
  * 5. Результат вызовов функции выведите в консоль
  */
+
+
+const helloName = function (name) {
+    return `Привет, ${name}!`;
+}
+
+console.log(helloName('Андрей'));
+console.log(helloName('Богдан'));
