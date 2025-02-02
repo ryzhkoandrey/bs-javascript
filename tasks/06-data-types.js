37c7c1190c7d4684404bfe9ac69ffde81f74de02
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-02 20:52:00 +0200

    6

--- tasks/06-data-types.js
+++ tasks/06-data-types.js
@@ -12,3 +12,20 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+
+const myString = 'String';
+const myNumber = 7;
+const myBoolean = true;
+const myNull = null;
+const myUndefined = undefined;
+const myObject = {};
+const myArray = [];
+
+console.log(myString, typeof myString);
+console.log(myNumber, typeof myNumber);
+console.log(myBoolean, typeof myBoolean);
+console.log(myNull, typeof myNull);
+console.log(myUndefined, typeof myUndefined);
+console.log(myObject, typeof myObject);
+console.log(myArray, typeof myArray);
