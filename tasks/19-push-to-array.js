0580cabfc1fec95c4b01027117425ff211a50c16
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-03 17:59:47 +0200

    19

--- tasks/19-push-to-array.js
+++ tasks/19-push-to-array.js
@@ -6,3 +6,9 @@
  *
  * 3. Выведите в консоль длину результирующего массива
  */
+
+const myArray = [1, 2, 3];
+console.log(myArray, ` length = ${myArray.length}`);
+
+myArray.push(4, 5);
+console.log(myArray, ` length = ${myArray.length}`);
