cc255b1330879533c31c8b49b5158e70411d6378
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-03 14:50:57 +0200

    12

--- tasks/12-create-object.js
+++ tasks/12-create-object.js
@@ -8,3 +8,11 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const person = {
+    name: 'Andrey',
+    surname: 'Ryzhko',
+    favoriteNumber: 1,
+}
+
+console.log(`My name is ${person.name} ${person.surname} and my favorite number is ${person.favoriteNumber}`);
