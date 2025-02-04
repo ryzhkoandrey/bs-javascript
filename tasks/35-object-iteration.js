8c7686e575d4d5a927c9e3c3d208f032e6454308
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-04 12:16:04 +0200

    35

--- tasks/35-object-iteration.js
+++ tasks/35-object-iteration.js
@@ -7,9 +7,26 @@
  */
 
 const myObject = {
-  key1: true,
-  key5: 10,
-  key3: 'abc',
-  key4: null,
-  key10: NaN,
+    key1: true,
+    key5: 10,
+    key3: 'abc',
+    key4: null,
+    key10: NaN,
 }
+
+// for (key in myObject) {
+//     if (myObject.hasOwnProperty(key)) {
+//         if (key === 'key1' || key === 'key3') {
+//             console.log(myObject[key]);
+//         }
+//     }
+// }
+
+const objectKeys = Object.keys(myObject);
+console.log(objectKeys);
+
+objectKeys.forEach((key) => {
+    if (key === 'key1' || key === 'key3') {
+        console.log(myObject[key]);
+    }
+});
