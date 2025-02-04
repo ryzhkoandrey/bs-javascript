de8d85939cdd48e0d6a12fbc1e1d39bb27d5c57a
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-04 11:51:24 +0200

    34

--- tasks/34-array-of-objects.js
+++ tasks/34-array-of-objects.js
@@ -11,3 +11,30 @@
  *
  * 4. Выведите результирующий массив в консоль
  */
+
+const cars = [
+    {
+        carBrand: 'Subaru',
+        price: 2500,
+        isAbailableForSale: true,
+    },
+    {
+        carBrand: 'BMW',
+        price: 3000,
+        isAbailableForSale: false,
+    },
+    {
+        carBrand: 'Audi',
+        price: 3500,
+        isAbailableForSale: true,
+    },
+];
+
+const newCar = {
+    carBrand: 'Porche',
+    price: 4000,
+    isAbailableForSale: false,
+}
+
+cars.push(newCar);
+console.log(cars);
