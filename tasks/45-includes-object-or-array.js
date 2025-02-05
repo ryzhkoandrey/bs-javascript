c65203d2e4e373e09af612546478f2dbeb0a35f7
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-05 21:57:45 +0200

    45

--- tasks/45-includes-object-or-array.js
+++ tasks/45-includes-object-or-array.js
@@ -9,16 +9,26 @@
  * а затем применить метод "includes" с аргументом, который также будет преобразован в строку
  */
 
+const isElementInArray = (searchElement, inputArray) => {
+    if (typeof searchElement !== 'object') {
+        return inputArray.includes(searchElement);
+    }
+
+    return inputArray
+        .map(el => JSON.stringify(el))
+        .includes(JSON.stringify(searchElement));
+}
+
 const tags = [
-  ['javascript', 'es6'],
-  ['css', 'flexbox'],
-  ['html', 'web-browser'],
+    ['javascript', 'es6'],
+    ['css', 'flexbox'],
+    ['html', 'web-browser'],
 ]
 
 const fruits = [
-  { title: 'Orange', quantity: 10 },
-  { title: 'Banana', quantity: 5 },
-  { title: 'Apple', quantity: 25 },
+    { title: 'Orange', quantity: 10 },
+    { title: 'Banana', quantity: 5 },
+    { title: 'Apple', quantity: 25 },
 ]
 
 const primitiveTypesArray = [25, 'x', true, undefined, null]
