74332ebfa495861eb890f18a55cf6e6f459d770d
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-05 22:36:05 +0200

    46

--- tasks/46-push-to-array-if-not-exists.js
+++ tasks/46-push-to-array-if-not-exists.js
@@ -10,6 +10,14 @@
  * ПРИМЕЧАНИЕ: Мы предполагаем, что "inputArray" содержит элементы только примитивных типов
  */
 
+function pushIfUnique(inputArray, newElement) {
+    if (inputArray.includes(newElement)) {
+        return console.log(`${newElement} уже находится в массиве`);
+    }
+
+    inputArray.push(newElement);
+}
+
 const myNumbers = [123, 50, 27]
 
 pushIfUnique(myNumbers, 50) // "50 уже в массиве"
