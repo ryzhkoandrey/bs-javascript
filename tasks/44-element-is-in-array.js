a92435f4baae0178a0c8091939130945e5462ae6
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-05 19:51:47 +0200

    44

--- tasks/44-element-is-in-array.js
+++ tasks/44-element-is-in-array.js
@@ -7,6 +7,8 @@
  * 3. В противном случае вернуть "false"
  */
 
+const isElementInArray = (inputArray, searchElement) => inputArray.includes(searchElement);
+
 const transports = ['Bus', 'Car', 'Bicycle', 'Airplane']
 
 console.log(isElementInArray(transports, 'Bus')) // true
