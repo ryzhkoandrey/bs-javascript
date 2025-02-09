8635b75ab41d4c1c1739cca4e823263bd231ef46
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-09 19:20:15 +0200

    59

--- tasks/59-extend-array.js
+++ tasks/59-extend-array.js
@@ -13,3 +13,20 @@
  * 4. Убедитесь, что остальные методы массивов такие,
  * как "forEach", "map" также доступны
  */
+
+class ExtendedArray extends Array {
+   sum() {
+      return this.reduce((sum, num) => (typeof num === 'number') ? (sum + num) : sum, 0);
+   }
+
+   onlyNumbers() {
+      return this.filter(item => typeof item === 'number');
+   }
+}
+
+const myArray = new ExtendedArray(1, 2, true, false, 3, 'a', undefined, null, 4, 5, 'b');
+
+console.log('myArray', myArray);
+console.log('myArray.sum()', myArray.sum());
+console.log('myArray.onlyNumbers()', myArray.onlyNumbers());
+console.log('myArray.length()', myArray.length);
