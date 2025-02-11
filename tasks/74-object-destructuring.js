a17629daf9f4f9a544cd6e1a2318ca22ddef022f
Андрей Рыжко <ryzhkoandrey@gmail.com>
2025-02-11 22:28:50 +0200

    74

--- tasks/74-object-destructuring.js
+++ tasks/74-object-destructuring.js
@@ -11,23 +11,35 @@
  */
 
 const person1 = {
-  name: 'Mike',
-  info: {
-    country: 'Spain',
-    age: 23,
-  },
-  postsQuantity: 100,
+   name: 'Mike',
+   info: {
+      country: 'Spain',
+      age: 23,
+   },
+   postsQuantity: 100,
 }
 
 const person2 = {
-  name: 'Alice',
-  info: {
-    country: 'Italy',
-    age: 25,
-  },
+   name: 'Alice',
+   info: {
+      country: 'Italy',
+      age: 25,
+   },
 }
 
 // Напишите функцию "shortPerson" здесь
+
+// const shortPerson = ({ name: n, info: { country: c, age: a }, postsQuantity: p = 0 }) => ({ n, c, a, p });
+
+const shortPerson = (person) => {
+   const {
+      name: n,
+      info: { country: c, age: a, },
+      postsQuantity: p = 0,
+   } = person;
+
+   return { n, c, a, p };
+}
 
 console.log(shortPerson(person1))
 // { n: "Mike", c: "Spain", a: 23, p: 100 }
